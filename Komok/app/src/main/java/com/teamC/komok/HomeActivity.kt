480ff4dc7f1faeca6cc4de9b265228b0dc5536ee
MRizaF <rizafarhandi@gmail.com>
2020-10-14 18:07:17 +0700

    add (about, login) layout and face detection

--- Komok/app/src/main/java/com/teamC/komok/HomeActivity.kt
+++ Komok/app/src/main/java/com/teamC/komok/HomeActivity.kt
@@ -3,14 +3,17 @@ package com.teamC.komok
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    companion object {
+        private const val IMAGE_PICK_CODE = 1000
+        private const val PERMISSION_CODE = 1001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -18,23 +21,15 @@ class HomeActivity : AppCompatActivity() {
         //val btnUpload: Button = findViewById(R.id.button_upload)
 
         findViewById<Button>(R.id.button_upload).setOnClickListener {
-            //check runtime permission
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED
-                ) {
-                    //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    //show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE)
-                } else {
-                    //permission already granted
-                    pickImageFromGallery()
-                }
-            } else {
-                //system OS is < Marshmallow
+            if (PermissionUtils.requestPermission(this, PERMISSION_CODE,
+                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 pickImageFromGallery()
             }
+        }
+
+        findViewById<Button>(R.id.button_about).setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -45,32 +40,11 @@ class HomeActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    companion object {
-        //image pick code
-        private const val IMAGE_PICK_CODE = 1000
-
-        //Permission code
-        private const val PERMISSION_CODE = 1001
-    }
-
     //handle requested permission result
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    //permission from popup granted
-                    pickImageFromGallery()
-                } else {
-                    //permission from popup denied
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (PermissionUtils.permissionGranted(requestCode, PERMISSION_CODE, grantResults)) {
+            pickImageFromGallery()
         }
     }
 
--- Komok/app/src/main/java/com/teamC/komok/SwapActivity.kt
+++ Komok/app/src/main/java/com/teamC/komok/SwapActivity.kt
@@ -2,16 +2,65 @@ package com.teamC.komok
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.IOException
 
 class SwapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_swap)
 
+        // get image upload from home activity
         val imageUpload: String? = intent.getStringExtra("imageUpload")
-
         findViewById<ImageView>(R.id.image_detect).setImageURI(Uri.parse(imageUpload))
+
+        findViewById<Button>(R.id.button_swap).setOnClickListener {
+            try {
+                val image = InputImage.fromFilePath(this, Uri.parse(imageUpload))
+                detectFaces(image)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun detectFaces(image: InputImage) {
+        val options = FaceDetectorOptions.Builder()
+            .setClassificationMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .setMinFaceSize(0.15f)
+            .enableTracking()
+            .build()
+
+        val detector = FaceDetection.getClient(options)
+
+        val test: TextView = findViewById(R.id.text_test)
+
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                var i = 0
+
+                for (face in faces) {
+                    val bounds = face.boundingBox
+                    i += 1
+                }
+
+                when (i) {
+                    1 -> test.text = "< $i FACE DETECTED >"
+                    else -> test.text = "< $i FACES DETECTED >"
+                }
+            }
+            .addOnFailureListener { e ->
+                test.text =  "< NO FACE DETECTED >"
+                // Task failed with an exception
+                // ...
+            }
     }
 }
